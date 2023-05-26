b9425c94b578ad92d88e282f8d4418428ab74f21
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-05-26 16:44:01 -0300

    requisito 3 ok

---
+++ script.js
@@ -0,0 +1,19 @@
+function validation(){
+    const inputEmail= document.querySelector('#email');
+    const inputPassword= document.querySelector('#password');
+
+    const button= document.querySelector('#buttonSubmit');
+
+        button.addEventListener('click',(e)=>{
+    
+            e.preventDefault();
+
+            if(inputEmail.value == 'tryber@teste.com' && inputPassword.value == '123456'){
+                alert('Olá, Tryber!')
+            }else{
+                alert('Email ou senha inválidos.')
+            }
+        })
+
+}
+validation();
