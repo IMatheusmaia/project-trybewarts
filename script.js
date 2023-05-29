cad9a1b5f72ad340d8c63829f2e79921e64387ba
IMatheusmaia <i.matheuslmaia@gmail.com>
2023-05-29 18:00:45 -0300

    melhorias de estilos e de funções

--- script.js
+++ script.js
@@ -12,9 +12,11 @@ function validation() {
     }
   });
 }
-const checkbox = document.querySelector('#agreement');
-const button = document.querySelector('#submit-btn');
-function TrueEfalse() {
+validation();
+
+function enableDesableButton() {
+  const button = document.querySelector('#submit-btn');
+  const checkbox = document.querySelector('#agreement');
   if (checkbox.checked) {
     button.disabled = false;
   } else {
@@ -22,5 +24,25 @@ function TrueEfalse() {
   }
 }
 
-validation();
-checkbox.addEventListener('change', TrueEfalse);
+function verifyCheckBox() {
+  const checkbox = document.querySelector('#agreement');
+  checkbox.addEventListener('change', enableDesableButton);
+}
+verifyCheckBox();
+
+function countCaracteres() {
+  const textArea = document.querySelector('textarea');
+  const countArea = document.querySelector('#counter');
+  const span = () => {
+    const beforeSpanText = document.querySelector('#counter > span');
+    if (beforeSpanText) {
+      beforeSpanText.remove();
+    } else {
+      const spanText = document.createElement('span');
+      spanText.innerText = `${textArea.textLength}`;
+      countArea.appendChild(spanText);
+    }
+  };
+  textArea.addEventListener('input', span);
+}
+countCaracteres();
